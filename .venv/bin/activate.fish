2c6c2d79ac42af4a42d26ab6521f9dc604189928
wonchul <onedang22@gmail.com>
2023-04-15 02:10:53 +0900

    update req.

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wonchul/github/graphql-tutorial/.venv"
+set -gx VIRTUAL_ENV "/home/wonchul/github/graphql-flask-sqlalchemy/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
