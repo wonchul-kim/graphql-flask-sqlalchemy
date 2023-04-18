90e3f11ce38685aed194db45d17e2a86832aba8c
wonchul <onedang22@gmail.com>
2023-04-18 11:01:23 +0900

    integrated database with postgres

--- mySqlalchemy/database.py
+++ mySqlalchemy/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker 
 
 # engine = create_engine('sqlite:///database.sqlite3')#, convert_unicode=True)
-engine = create_engine('postgresql://aiv:aiv11011@192.168.11.177/aivdl')
+engine = create_engine('postgresql://aiv:aiv11011@192.168.10.41/aivdl')
 
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
